5c04956d928aa6e10485346b2cb345cd2bec6bf1
Hijondo <hijondo@gmail.com>
2017-10-15 11:38:49 -0700

    Better index.vue

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-layout row wrap>
+    <!-- About -->
+    <v-flex xs12 md6 offset-md3 lg4 offset-lg4>
+      <div class="text-xs-center">
+        <img src="/hijondo.png" alt="Hijondo" width=100 class="mb-5">
+      </div>
+
+      <blockquote>
+        &#8220;It sorta sounds like 'Hi, John Doe.'&#8221;
+        <footer>
+          <small>
+            <em>&mdash;Hijondo</em>
+          </small>
+        </footer>
+      </blockquote>
+    </v-flex>
+  </v-layout>
+</template>
